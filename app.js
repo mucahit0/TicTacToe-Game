d2d7aeb76ececddf97ba8ad3c759d2cbbbfcf3f3
Mücahit <mucahittasan00@gmail.com>
2022-04-16 12:06:51 +0300

    Draw area is updated

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const playerText = document.getElementById('player-text');
 const button = document.getElementById('restart');
 
 let player = "X";
+let count = 0;
 
 const winningConditions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 const startGame = () => {
 
@@ -22,56 +23,68 @@ const startGame = () => {
 
 }
 
-function chooseArea(box){
-    if(box.textContent === ""){
+function chooseArea(box) {
+    if (box.textContent === "") {
         box.textContent = player;
         turnPlayer();
-    }else{
+        count++;
+    } else {
         return;
+        count++;
     }
-    
+
     checkWinner();
+    console.log(count)
 }
 
-function turnPlayer(){
-    if(player === "X"){
+function turnPlayer() {
+    if (player === "X") {
         player = "O";
         playerText.textContent = `${player}'s turn`;
-    }else{
+    } else {
         player = "X";
         playerText.textContent = `${player}'s turn`;
     }
-}    
+}
 
-function checkWinner(){ 
-    
+function checkWinner() {
+
     let allBox = boxes;
-    
-    for(let i = 0; i < winningConditions.length; i++){
+
+    for (let i = 0; i < winningConditions.length; i++) {
         let a = winningConditions[i][0];
         let b = winningConditions[i][1];
         let c = winningConditions[i][2];
-        
-        if(allBox[a].textContent === allBox[b].textContent && allBox[a].textContent === allBox[c].textContent && allBox[a].textContent !== ""){
+
+        const condition = allBox[a].textContent === allBox[b].textContent && allBox[a].textContent === allBox[c].textContent && allBox[a].textContent !== "";
+        console.log(condition)
+
+        if (condition) {
             playerText.textContent = `${allBox[a].textContent} is the winner!`
             player = 'X'
             button.style.display = 'block';
-        }else if(count === 9){ // if all boxes are filled and no winner
+        } else if (count === 9 && !condition) { // if all boxes are filled and no winner
             playerText.textContent = "It's a draw!"
             button.style.display = 'block';
-        }    
+        }
+        else if (count === 9 && condition) { // if all boxes are filled and no winner
+            playerText.textContent = `${allBox[a].textContent} is the winner!`
+            player = 'X'
+            button.style.display = 'block';
+        }
 
-    }    
-    
-}    
+    }
 
-button.addEventListener('click',()=>{
+}
+
+button.addEventListener('click', () => {
     clearBoxes();
     startGame();
     button.style.display = 'none';
+    count = 0;
 })
 
-function clearBoxes(){
+function clearBoxes() {
     boxes.forEach(box => box.textContent = "");
 }
 
